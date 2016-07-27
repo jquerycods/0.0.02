40add9ecef4624af5c7427902637ae97a0067166
jquerycods <cizgifilmhikayeleri@gmail.com>
2016-07-27 19:58:01 +0300

    Add files via upload

---
+++ news.js
@@ -0,0 +1,3 @@
+﻿// Blogger Eklentileri URL=(http://bloggereklentileri.blogspot.com/)
+var numposts=1,numposts2=6,showpostthumbnails=!0,showpostthumbnails2=!1,displaymore=!1,displaymore2=!1,showcommentnum=!0,showcommentnum2=!1,showpostdate=!0,showpostdate2=!0,showpostsummary=!0,numchars=150,thumb_width=280,thumb_height=200,thumb_width2=62,thumb_height2=62,no_thumb=&quot;https://cdn.rawgit.com/jquerycods/yeni/master/yenibe.png&quot;,no_thumb2=&quot;https://cdn.rawgit.com/jquerycods/be/master/be.png&quot;;
+// Çizgi Film Hikayeleri URL=(http://cizgifilmhikayeleri.blogspot.com/)
